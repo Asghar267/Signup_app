69b776c61ed7b555fcba0f45c3e4c1774f361a72
asghar267 <asgharabbasi267@gmail.com>
2024-08-19 08:40:38 +0500

    Signup Login Android App

--- Mobile_app/app/src/main/java/com/example/mobile_app/LoginActivity.java
+++ Mobile_app/app/src/main/java/com/example/mobile_app/LoginActivity.java
@@ -6,19 +6,25 @@ import android.util.Patterns;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
     TextView regbtn;
     TextInputEditText emailtxt, passwordtxt;
     MaterialButton loginBtn;
+
+    FirebaseAuth auth;
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,8 @@ public class LoginActivity extends AppCompatActivity {
 //        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
         regbtn = this.findViewById(R.id.reg_id);
         emailtxt = this.findViewById(R.id.email_id);
         passwordtxt = this.findViewById(R.id.password_id);
@@ -59,7 +67,38 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(this, "Password is Required!", Toast.LENGTH_SHORT).show();
             return;
         }
-        Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
+        auth.signInWithEmailAndPassword(emailtxt.getText().toString(), passwordtxt.getText().toString()).addOnSuccessListener(this, new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+
+                firestore.collection("users").document(authResult.getUser().getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Toast.makeText(LoginActivity.this, documentSnapshot.getString("name") ,Toast.LENGTH_SHORT).show();
+
+                        Intent logIntent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(logIntent);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+
+            }
+        }).addOnFailureListener(LoginActivity.this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e.getMessage().contains("The password is invalid") || e.getMessage().contains("There is no user record")) {
+                    Toast.makeText(LoginActivity.this, "Incorrect email or password.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
--- Mobile_app/app/src/main/java/com/example/mobile_app/SignupActivity.java
+++ Mobile_app/app/src/main/java/com/example/mobile_app/SignupActivity.java
@@ -1,69 +1,136 @@
 package com.example.mobile_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
 
     TextInputEditText nametxt, phonetxt, emailtxt, passwordtxt;
     MaterialButton signupBtn;
 
+    FirebaseAuth auth;
+    FirebaseFirestore firestore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
+        FirebaseApp.initializeApp(this);
 
-        nametxt = this.findViewById(R.id.name);
-        phonetxt = this.findViewById(R.id.phone);
+        auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+
+        nametxt = this.findViewById(R.id.name_id);
+        phonetxt = this.findViewById(R.id.phone_id);
         emailtxt = this.findViewById(R.id.email_id);
         passwordtxt = this.findViewById(R.id.password_id);
-
-        signupBtn = this.findViewById(R.id.signup_btn);
+        signupBtn = this.findViewById(R.id.signupbtn_id);
 
         signupBtn.setOnClickListener(v ->{
             validateSignup();
-        } );
+        });
 
     }
 
     public void validateSignup(){
 
-        if (nametxt.getText().toString().isEmpty()){
+        String name = nametxt.getText().toString();
+        String phone = phonetxt.getText().toString();
+        String email = emailtxt.getText().toString();
+        String password = passwordtxt.getText().toString();
+
+        if (name.isEmpty()){
             Toast.makeText(this, "Name Is Null", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if(phonetxt.getText().toString().isEmpty()){
+        if(phone.isEmpty()){
             Toast.makeText(this, "Phone is null", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if(emailtxt.getText().toString().isEmpty()){
+        if(email.isEmpty()){
             Toast.makeText(this, "Email is null", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if (!Patterns.EMAIL_ADDRESS.matcher(emailtxt.getText().toString()).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
             Toast.makeText(this, "Email is Not Valid", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if(passwordtxt.getText().toString().isEmpty()){
+        if(password.isEmpty()){
             Toast.makeText(this, "Password is empty", Toast.LENGTH_SHORT).show();
             return;
         }
 
+
+        Toast.makeText(this, "Validate Success", Toast.LENGTH_SHORT).show();
+        auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(this, new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", authResult.getUser().getUid());
+                map.put("name", name);
+                map.put("phone", phone);
+                map.put("email", email);
+
+//                firestore.collection("users").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+              ////  both work but below is for to get user id
+                firestore.collection("users").document(authResult.getUser().getUid()).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                        Toast.makeText(SignupActivity.this, "User is Created", Toast.LENGTH_SHORT).show();
+                        Intent sinIntent = new Intent(SignupActivity.this, LoginActivity.class);
+                        startActivity(sinIntent);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignupActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+                return;
+
+            }
+        }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("SignupActivity", "Signup failed", e);
+                Toast.makeText(SignupActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                return;
+            }
+
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Log.e("SignupActivity", "User creation failed", e);
+//
+//                Toast.makeText(SignupActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//            }
+        });
 
     }
 }
